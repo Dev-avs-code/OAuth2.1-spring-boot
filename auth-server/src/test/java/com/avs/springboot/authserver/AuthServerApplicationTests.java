86e5a3c6693b401ba926220d6f77b00dd672ca3d
AVS7777 <arturo02nov@gmail.com>
2025-03-19 12:38:06 -0600

    Implement Spring Authorization Server OAuth 2.1 JWT

---
+++ auth-server/src/test/java/com/avs/springboot/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avs.springboot.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
